451d3e09ae1692d972c5acfecd7933b4a5a73d06
RuebenHales <halesrueben@gmail.com>
2022-03-01 14:47:04 -0500

    sending jwt on login

--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -1,15 +1,20 @@
 import { useState, } from "react"
-export default function Signup ({ setToken, setIsUser }) {
+import bcrypt from "bcryptjs"
+
+const salt = '$2b$10$WaC2/VZrG3qSl15nAfN0Pu'
+
+export default function Login({ setToken, setIsUser }) {
     const[email,setEmail] = useState('')
     const[password,setPassword] = useState('')
     const handleSubmit = (e) =>{
         e.preventDefault()
+        const hashedPassword = bcrypt.hashSync(password, salt)
      fetch('http://localhost:3001/users',{
          method: "POST",
          headers: {
              'Content-Type': 'application/json'
          },
-         body:JSON.stringify({email,password})
+         body:JSON.stringify({email, password: hashedPassword})
      })   
     .then(response => response.json())
         .then(data => setToken(data.token))
